ecdad4226a28e9b727112c4762efe7669ccf03d5
Luca Stefani <luca.stefani.ge1@gmail.com>
2025-03-14 17:30:33 +0000

    sm6150-common: Don't build vndk_package * It's built by default if vndk is set to current

--- common.mk
+++ common.mk
@@ -466,10 +466,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     prebuilts/vndk/v30/arm64/arch-arm64-armv8-a/shared/vndk-sp/libutils.so:$(TARGET_COPY_OUT_VENDOR)/lib64/libutils-v30.so
 
-# VNDK-SP
-PRODUCT_PACKAGES += \
-    vndk_package
-
 # Wifi
 PRODUCT_PACKAGES += \
     android.hardware.wifi-service \
